5f9e40a8e28bf3597d95202fe34fd0f2375d8c3c
rualt <rualnar@gmail.com>
2019-11-17 20:11:59 +0400

    fix route ussers/new, add file sourse json

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@
 use Slim\Factory\AppFactory;
 use DI\Container;
 use function Stringy\create as s;
-use function HP\Parser\csvToArray;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$users = csvToArray(';', '"', __DIR__ . '/../data/characters.csv');
+$file = file_get_contents(__DIR__ . '/../data/characters.json');
+$users = json_decode($file, true);
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -46,7 +46,7 @@ $app->get('/404', function ($request, $response) use ($router) {
 
 $app->get('/users', function ($request, $response) use ($router, $users) {
     $urlUsers = $router->urlFor('users');
-    $urlAddUser = $router->urlFor('add user');
+    $urlNewUser = $router->urlFor('new user');
     $term = $request->getQueryParam('term');
     $result = collect($users)->sortBy('Name')->filter(function ($user) use ($term) {
         return s($user['Name'])->startsWith($term, false);
@@ -55,10 +55,18 @@ $app->get('/users', function ($request, $response) use ($router, $users) {
         'users' => $result,
         'term' => $term,
         'urlUsers' => $urlUsers,
-        'urlAddUser' => $urlAddUser
+        'urlNewUser' => $urlNewUser
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 })->setName('users');
+
+$app->get('/users/new', function ($request, $response) use ($router, $users) {
+    $params = [
+        'user' => ['name' => '', 'email' => '', 'password' => '', 'passwordConfirmation' => '', 'city' => ''],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+})->setName('new user');
 
 $app->get('/users/{id}', function ($request, $response, $args) use ($users) {
     $id = $args['id'];
@@ -70,14 +78,6 @@ $app->get('/users/{id}', function ($request, $response, $args) use ($users) {
     $newResponse = isset($user) ? $response : $response->withStatus(404)->withHeader('Location', '/404');
     return $this->get('renderer')->render($newResponse, 'users/show.phtml', $params);
 })->setName('user');
-
-$app->get('/add/user', function ($request, $response) use ($router, $users) {
-    $params = [
-        'user' => ['name' => '', 'email' => '', 'password' => '', 'passwordConfirmation' => '', 'city' => ''],
-        'errors' => []
-    ];
-    return $this->get('renderer')->render($response, "add/user.phtml", $params);
-})->setName('add user');
 
 /* S$app->post('/users', function ($request, $response) use ($repo) {
     $validator = new Validator();
